4ba761e3a685f353a582c4e2134a3152702b44a0
Pycodd123 <94187777+Pycodd123@users.noreply.github.com>
2022-12-10 17:33:46 -0800

    camerappsuccesful!

---
+++ KivyMobileApp/cameraapp.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.camera import Camera
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+import os 
+import ffmpeg
+os.environ["KIVY_GL_BACKEND"]="gl"
+import kivy
+class Mainapp(App):
+    def build(self):
+        layout=BoxLayout(orientation="vertical")
+        self.camera= Camera(play=True)
+        self.camera.play=True
+        self.camera.resolution=(300,300)
+        self.click=Button(text="Take photo",size_hint=(.5,.2),pos_hint={"x":.25,"y":.75})
+       # self.click.bind(on_press=self.onclick)
+        layout.add_widget(self.camera)
+        layout.add_widget(self.click)
+        return layout
+
+
+Mainapp().run()        
