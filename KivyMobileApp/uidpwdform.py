b68396b32477696b086ceb8cc24fc71efa161da4
Pycodd123 <111271071+interestingstuf@users.noreply.github.com>
2023-01-08 19:16:18 -0800

    Edits
    Added Standard Credentials Security

--- KivyMobileApp/uidpwdform.py
+++ KivyMobileApp/uidpwdform.py
@@ -7,10 +7,15 @@ class MyGrid(Widget):
     uid=ObjectProperty(None)
     pwd=ObjectProperty(None)
     def login(self):
-        
-        print("Login Succesful With Credentials: ","User ID:",self.uid.text,"Password:",self.pwd.text)
-        self.uid.text=" "
-        self.pwd.text=" "
+        uid1=self.uid.text
+        pwd1=self.pwd.text
+        if uid1 == "Parth" and pwd1 == "1892601":
+            #print("Login Succesful With Credentials: ","User ID:",self.uid.text,"Password:",self.pwd.text)
+            print("Login Succesful With Given Credentials")
+            self.uid.text=" "
+            self.pwd.text=" "
+        else:
+            print("Login Credetials Rejected")    
 class newfile3(App):
     def build(self):
         return MyGrid()
